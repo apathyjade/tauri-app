44c0d4a2341dcce979be5baf45a20fb6db404046
jade <apathyjade@outlook.com>
2025-02-14 18:20:46 +0800

    feat: init

--- src/components/LexicalEditor/index.tsx
+++ src/components/LexicalEditor/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { LexicalComposer } from '@lexical/react/LexicalComposer';
 import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
 import { ContentEditable } from '@lexical/react/LexicalContentEditable';
@@ -7,7 +7,7 @@ import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
 import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary';
 import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
 
-import {HorizontalRulePlugin} from '@lexical/react/LexicalHorizontalRulePlugin';
+import { HorizontalRulePlugin } from '@lexical/react/LexicalHorizontalRulePlugin';
 
 import ToolBar from './plugins/ToolBar';
 import $css from './index.module.scss';
@@ -16,13 +16,15 @@ import theme from './themes/StickyEditorTheme';
 import { ToolbarContext } from './context/ToolbarContext';
 import { HeadingNode, QuoteNode } from '@lexical/rich-text';
 import { TableCellNode, TableNode, TableRowNode } from '@lexical/table';
-import {HorizontalRuleNode} from '@lexical/react/LexicalHorizontalRuleNode';
+import { HorizontalRuleNode } from '@lexical/react/LexicalHorizontalRuleNode';
 import classNames from 'classnames';
 import { ImageNode } from './nodes/ImgNode/ImageNode';
 import { LineDividerNode } from './nodes/LineDivider';
 import { ListItemNode, ListNode } from '@lexical/list';
 import { CodeHighlightNode, CodeNode } from '@lexical/code';
-import { EditorState } from 'lexical';
+import { $getRoot, $insertNodes, CLEAR_HISTORY_COMMAND, EditorState, LexicalEditor, LexicalUpdateJSON } from 'lexical';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { $generateHtmlFromNodes } from '@lexical/html';
 
 
 const initialConfig = {
@@ -43,39 +45,58 @@ const initialConfig = {
     HorizontalRuleNode,
   ] as any,
   onError: (error: Error) => {
-    console.error(error);
   },
 };
 
+interface Props {
+  value?: any;
+  onChange: (editorStateJson: object) => void;
+}
 
-function Editor() {
+
+function Editor({ value, onChange }: Props) {
   const [isFull, setFull] = useState(false);
-
-  const onChange = useCallback((editorState: EditorState) => {
-    editorState.read(() => {
-      console.log(editorState.toJSON());
-    });
+  const [editor] = useLexicalComposerContext();
+  useEffect(() => {
+    if (value?.root) {
+      const editorState = editor.parseEditorState(value);
+      editor.setEditorState(editorState);
+      editor.dispatchCommand(CLEAR_HISTORY_COMMAND, undefined);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const _onChange = useCallback((editorState) => {
+    editorState.read(() => {
+      onChange(editorState.toJSON());
+    });
+  }, [onChange]);
+
   return (
-    <LexicalComposer initialConfig={initialConfig}>
-      <div className={classNames($css.container, isFull ? $css.full : undefined)}>
-        <ToolbarContext isFull={isFull} setFull={setFull}>
-          <ToolBar />
-          <RichTextPlugin
-            contentEditable={<ContentEditable className={$css['editor-warp']} />}
-            placeholder={null}
-            ErrorBoundary={LexicalErrorBoundary}
-          />
-          {/* <MarkdownShortcutPlugin transformers={TRANSFORMERS} /> */}
-          <OnChangePlugin onChange={onChange} />
-          <HistoryPlugin />
-          <AutoFocusPlugin />
-          <HorizontalRulePlugin />
-        </ToolbarContext>
-      </div>
-    </LexicalComposer>
+    <div className={classNames($css.container, isFull ? $css.full : undefined)}>
+      <ToolbarContext isFull={isFull} setFull={setFull}>
+        <ToolBar />
+        <RichTextPlugin
+          contentEditable={
+            <div className={$css['editor-warp']}>
+              <ContentEditable className={$css['editor-content']} />
+            </div>
+          }
+          placeholder={null}
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+        {/* <MarkdownShortcutPlugin transformers={TRANSFORMERS} /> */}
+        <OnChangePlugin onChange={_onChange} />
+        <HistoryPlugin />
+        <AutoFocusPlugin />
+        <HorizontalRulePlugin />
+      </ToolbarContext>
+    </div>
   );
 }
 
-export default Editor;
+export default (props) => {
+  return <LexicalComposer initialConfig={initialConfig}>
+    <Editor {...props} />
+  </LexicalComposer>;
+};
